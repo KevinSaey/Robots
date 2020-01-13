fdd500e1d6b58b14a3ba042665a54a306ee1a575
KevinSaey <36826320+KevinSaey@users.noreply.github.com>
2020-01-13 20:06:46 +0000

    Added UnrealRemote

---
+++ Robots/CommunicationTCPIP.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+//code adapted from https://docs.microsoft.com/en-us/dotnet/framework/network-programming/asynchronous-server-socket-example
+namespace Robots
+{
+    public class ServerTCPIP
+    {
+        TcpListener _server;
+        TcpClient _client;
+        NetworkStream _networkStream;
+        byte[] _bytes = new byte[6 * 4];
+
+        ServerTCPIP(IPAddress ip, int port)
+        {
+            _server = new TcpListener(ip, port);
+            _server.Start();
+
+        }
+
+        public async Task Connect()
+        {
+            try
+            {
+                _client = await _server.AcceptTcpClientAsync();
+
+                NetworkStream _networkStream = _client.GetStream();
+                Console.WriteLine($"Server connected to a client");
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine($"SocketException: {e}");
+            }
+        }
+
+        public async Task TrySendJoints(float[] joints)
+        {
+            try
+            {
+                Buffer.BlockCopy(joints, 0, _bytes, 0, _bytes.Length);
+                await _networkStream.WriteAsync(_bytes, 0, _bytes.Length);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Exeption:{e.Message}");
+
+            }
+        }
+    }
+    /*public class ServerTCPIP
+    {
+        System.Net.Sockets.
+    }
+
+
+    public class ObjectState
+    {
+        public Socket wSocket = null;
+        public const int bufferSize = 1024;
+        public byte[] buffer = new byte[bufferSize];
+        public StringBuilder sb = new StringBuilder();
+
+    }
+
+    public class AsyncSocketListener
+    {
+        public static ManualResetEvent allCompleted = new ManualResetEvent(false);
+        public static void StartListener()
+        {
+            byte[] bytes = new byte[1024];
+
+            IPHostEntry ipHost = ipHost = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ip = ipHost.AddressList[0];
+            IPEndPoint localEndPoint = new IPEndPoint(ip, 4343);
+            Socket listener = new Socket(ip.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                listener.Bind(localEndPoint);
+                listener.Listen(100);
+
+                while (true)
+                {
+                    allCompleted.Reset();
+                    Console.WriteLine($"Waiting for incomming connections.");
+                    listener.BeginAccept(new AsyncCallback(AcceptCallback), listener);
+                    allCompleted.WaitOne();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+        private static void AcceptCallback(IAsyncResult ar)
+        {
+            allCompleted.Set();
+            Socket listener = (Socket)ar.AsyncState;
+            Socket handler = listener.EndAccept(ar);
+
+            ObjectState state = new ObjectState();
+            state.wSocket = handler;
+            handler.BeginReceive(state.buffer, 0, ObjectState.bufferSize, 0, new AsyncCallback(ReadCallback), state);
+
+        }
+
+        private static void ReadCallback(IAsyncResult ar)
+        {
+            string content = String.Empty;
+            ObjectState state = (ObjectState)ar.AsyncState;
+            Socket handler = state.wSocket;
+            int bytesRead = handler.EndReceive(ar);
+            if (bytesRead > 0)
+            {
+                state.sb.Append(Encoding.ASCII.GetString(state.buffer, 0, bytesRead));
+                content = state.sb.ToString();
+                if (content.IndexOf("<EOF>", StringComparison.Ordinal) > -1)
+                {
+                    Console.WriteLine($"Read: {content.Length} bytes from socket Data: {content}");
+                    Send(handler, content);
+                }
+                else
+                {
+                    handler.BeginReceive(state.buffer, 0, ObjectState.bufferSize, 0, new AsyncCallback(ReadCallback), state);
+                }
+            }
+        }
+
+        private static void Send(Socket handler, string content)
+        {
+            byte[] byteData = Encoding.ASCII.GetBytes(content);
+            handler.BeginSend(byteData, 0, byteData.Length, 0, new AsyncCallback(SendCallback), handler);
+        }
+
+        private static void SendCallback(IAsyncResult ar)
+        {
+            try
+            {
+                Socket handler = (Socket)ar.AsyncState;
+                int byteSent = handler.EndSend(ar);
+                Console.WriteLine($"Sent: {byteSent} to client");
+
+                handler.Shutdown(SocketShutdown.Both);
+                handler.Close();
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e);
+            }
+        }
+    }*/
+}
---
+++ RobotsGH/UnrealRemote.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using System.Threading;
+using System.Threading.Tasks;
+
+using System.Net;
+using System.Net.Sockets;
+
+using Grasshopper.Kernel;
+using Grasshopper.Kernel.Types;
+using Rhino.Geometry;
+
+namespace Robots.Grasshopper
+{
+    public class UnrealRemote : GH_Component
+    {
+        TcpListener _server;
+        TcpClient _client;
+        NetworkStream _networkStream;
+        byte[] _bytes = new byte[6 * 4];
+        List<string> _log = new List<string>();
+        bool _connect, _play, _pause;
+
+        public UnrealRemote() : base("UnrealRemote", "UE Remote", "Connect and stream to Unreal engine", "Robots", "Components") { }
+        public override Guid ComponentGuid => new Guid("0f69c3b9-a687-45e6-a250-d015d7df7f26");
+        protected override System.Drawing.Bitmap Icon => Properties.Resources.iconURRemote;
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddParameter(new ProgramParameter(), "Joints", "J", "Program", GH_ParamAccess.item);
+            pManager.AddTextParameter("IP", "IP", "IP address of the server", GH_ParamAccess.item);
+            pManager.AddBooleanParameter("Connect", "C", "Connect Unreal", GH_ParamAccess.item, false);
+            pManager.AddBooleanParameter("Play", "P", "Play", GH_ParamAccess.item, false);
+            pManager.AddBooleanParameter("Pause", "S", "Pause", GH_ParamAccess.item, false);
+            for (int i = 0; i < 5; i++)
+            {
+                pManager[i].Optional = true;
+            }
+        }
+
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddTextParameter("Log", "L", "Log", GH_ParamAccess.list);
+        }
+
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            GH_String ip = null;
+
+            if (!DA.GetData("IP", ref ip)) { _log.Add("No IP found"); }
+            if(DA.GetData("Connect", ref _connect))
+            {
+
+            }
+
+            DA.SetDataList("Log", _log);
+        }
+
+
+        protected async void Connect(IPAddress ip, int port)
+        {
+            _server = new TcpListener(ip, port);
+            _server.Start();
+
+            await AsyncConnect();
+        }
+
+        public async Task AsyncConnect()
+        {
+            try
+            {
+                _client = await _server.AcceptTcpClientAsync();
+
+                NetworkStream _networkStream = _client.GetStream();
+                _log.Add($"Server connected to a client");
+            }
+            catch (SocketException e)
+            {
+                _log.Add($"SocketException: {e}");
+            }
+        }
+
+        public async Task AsyncSendJoints(float[] joints)
+        {
+            try
+            {
+                Buffer.BlockCopy(joints, 0, _bytes, 0, _bytes.Length);
+                await _networkStream.WriteAsync(_bytes, 0, _bytes.Length);
+            }
+            catch (Exception e)
+            {
+                _log.Add($"Exeption:{e.Message}");
+            }
+        }
+    }
+}
